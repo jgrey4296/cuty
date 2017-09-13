865c4f8e1d133c659f99705653c8f060a50411d6
John Grey <jgrey@ucsc.edu>
2017-09-13 18:44:57 +0100

    refactored make_gif to be an easy to use class

--- cairo_utils/make_gif.py
+++ cairo_utils/make_gif.py
@@ -1,61 +1,49 @@
-
-#---------- Logging
-import logging as root_logger
-LOGLEVEL = root_logger.DEBUG
-logFileName = "gif_write.log"
-root_logger.basicConfig(filename=logFileName, level=LOGLEVEL, filemode='w')
-
-console = root_logger.StreamHandler()
-console.setLevel(root_logger.INFO)
-root_logger.getLogger('').addHandler(console)
-logging = root_logger.getLogger(__name__)
-#---------- Imports
 import sys
 from os.path import isfile,exists,join, getmtime, splitext
 from os import listdir
 from PIL import Image,ImageSequence
 import imageio
 import re
+import logging as root_logger
 
-#---------- Constants
-GIF_OUTPUT_DIR = "."
-GIF_NAME = "anim.gif"
+logging = root_logger.getLogger(__name__)
 
-SOURCE_DIR = 'images'
-FILE_FORMAT = '.png'
-FPS=12
-numRegex = re.compile(r'(\d+)')
+class Make_Gif:
+    """ A Utility class to easily create gifs from a number of images """
+    
+    def __init__(self, output_dir=".", gif_name="anim.gif", source_dir="images", file_format=".png", fps=12):
+        self.output_dir = output_dir
+        self.gif_name = gif_name
+        self.source_dir = source_dir
+        self.file_format = file_format
+        self.fps = fps
 
-#---------- Utility
-def getNum(s):
-    """ Given a String, extract a number from it,
-    or return a default """
-    logging.info("Getting num of: {}".format(s))
-    try:
-        return int(numRegex.search(s).group(0))
-    except Exception:
-        return 9999999
-#----------
-# NOTE: THE BELOW CODE SHOULD NOT BE INDENTED
+        self.numRegex = re.compile(r'(\d+)')
 
-# Get all Files
-files = [x for x in listdir(SOURCE_DIR) if isfile(join(SOURCE_DIR,x))]
-# Sort by the number extracted from the filename
-files.sort(key=lambda x: getNum(x))
+    def getNum(self, s):
+        """ Given a String, extract a number from it,
+        or return a default """
+        logging.info("Getting num of: {}".format(s))
+        assert(isinstance(s, str))
+        try:
+            return int(self.numRegex.search(s).group(0))
+        except Exception:
+            return 9999999
 
-# Load the images
-# images = []
-# for filename in files:
-#     logging.info("Loading: {}".format(filename))
-#     images.append(imageio.imread(join(SOURCE_DIR,filename)))
+    def run(self):
+        # Get all Files
+        files = [x for x in listdir(SOURCE_DIR) if isfile(join(SOURCE_DIR,x))]
+        assert(bool(files))
+        logging.info("Making gif of {} frames".format(len(files)))
 
+        # Sort by the number extracted from the filename
+        files.sort(key=lambda x: getNum(x))
 
-# Export as a Gif
-#imageio.mimsave(join(GIF_OUTPUT_DIR,GIF_NAME),images,'GIF')
-
-#Alternative, for when all the images can't fit in memory:
-with imageio.get_writer(join(GIF_OUTPUT_DIR,GIF_NAME), mode='I') as writer:
-    for filename in files:
-        image = imageio.imread(join(SOURCE_DIR,filename))
-        writer.append_data(image)
+        # Export as a Gif
+        logging.info("Starting GIF writing")
+        with imageio.get_writer(join(GIF_OUTPUT_DIR,GIF_NAME), mode='I') as writer:
+            for filename in files:
+                image = imageio.imread(join(SOURCE_DIR,filename))
+                writer.append_data(image)
+        logging.info("Finished GIF writing")
 
