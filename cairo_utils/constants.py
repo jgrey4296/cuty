02b0898ee57fa53020fdebcde2bc72a7688c24e1
John Grey <jgrey@ucsc.edu>
2018-04-23 18:58:13 +0100

    adjusted constants for larger scale of cairo usage, instead of normalized 0-1 range

--- cairo_utils/constants.py
+++ cairo_utils/constants.py
@@ -10,14 +10,16 @@ logging = root_logger.getLogger(__name__)
 
 #constants:
 #BBOX = [min_x, min_y, max_x, max_y]
-SMALL_RADIUS = 0.003
-FONT_SIZE = 0.03
+SMALL_RADIUS = 15
+FONT_SIZE = 100
+WIDTH = 10
 ALPHA = 0.1
 TWOPI = 2 * pi
 QUARTERPI = 0.5 * pi
 THREEFOURTHSTWOPI = 3/4 * TWOPI
 EPSILON = sys.float_info.epsilon
 D_EPSILON = EPSILON * 20
+TOLERANCE = 0.5
 
 TEXT = [0, 1, 1, 1]
 EDGE = [1, 0, 0, 1]
