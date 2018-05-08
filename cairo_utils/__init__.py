997ed362aeacf01650e9363b63ef1ee6dc297a63
John Grey <jgrey@ucsc.edu>
2018-05-08 18:15:30 +0100

    removed obsolete beachline import in __init__

--- cairo_utils/__init__.py
+++ cairo_utils/__init__.py
@@ -2,7 +2,6 @@ from .Quadratic import Quadratic
 from .Parabola import Parabola
 from .Tree import Tree
 from .rbtree.RBTree import RBTree
-from .beachline.BeachLine import BeachLine
 from .dcel.dcel import DCEL
 from .make_gif import Make_Gif
 
