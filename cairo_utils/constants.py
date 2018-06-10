3779e5fecc0fdb83afefd59aa071a0d9890702f4
John Grey <jgrey@ucsc.edu>
2018-06-10 20:43:27 +0100

    switched constants over to numpy arrays instead of lists

--- cairo_utils/constants.py
+++ cairo_utils/constants.py
@@ -3,6 +3,7 @@
 """
 from math import pi
 from enum import Enum
+import numpy as np
 import logging as root_logger
 import sys
 
@@ -14,6 +15,7 @@ SMALL_RADIUS = 15
 FONT_SIZE = 100
 WIDTH = 10
 ALPHA = 0.1
+PI = pi
 TWOPI = 2 * pi
 QUARTERPI = 0.5 * pi
 THREEFOURTHSTWOPI = 3/4 * TWOPI
@@ -21,13 +23,15 @@ EPSILON = sys.float_info.epsilon
 D_EPSILON = EPSILON * 20
 TOLERANCE = 0.5
 
-TEXT = [0, 1, 1, 1]
-EDGE = [1, 0, 0, 1]
-VERTEX = [1, 0, 1, 1]
-FACE = [0, 0, 1, 1]
-START = [0, 1, 0, 1]
-END = [1, 0, 0, 1]
-BACKGROUND = [0, 0, 0, 1]
-FRONT = [0.8, 0.1, 0.71, ALPHA]
+VERTRAD = 10
+
+TEXT = np.array([0, 1, 1, 1])
+EDGE = np.array([1, 0, 0, 1])
+VERTEX = np.array([1, 0, 1, 1])
+FACE = np.array([0, 0, 1, 1])
+START = np.array([0, 1, 0, 1])
+END = np.array([1, 0, 0, 1])
+BACKGROUND = np.array([0, 0, 0, 1])
+FRONT = np.array([0.8, 0.1, 0.71, ALPHA])
 
 IntersectEnum = Enum("BBox Intersect Edge", "VLEFT VRIGHT HTOP HBOTTOM")
