b3ef8879957108070001c4230af3bfdb785f60fc
John Grey <jgrey@ucsc.edu>
2018-07-01 07:57:49 +0100

    added auto imports of sample specs

--- cairo_utils/dcel/sample_specs/__init__.py
+++ cairo_utils/dcel/sample_specs/__init__.py
@@ -1,2 +1,5 @@
 from .SampleSpec import SampleSpec
 from .VectorSample import VectorSample
+from .CircleSample import CircleSample
+from .TargetSample import TargetSample
+from .AngleSample import AngleSample
