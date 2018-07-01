e6471e311b81a4c1125cf73347354feee5f579f3
John Grey <jgrey@ucsc.edu>
2018-07-01 07:57:01 +0100

    added dcel sample spec abstract class, and simplified vector sample class

--- cairo_utils/dcel/sample_specs/SampleSpec.py
+++ cairo_utils/dcel/sample_specs/SampleSpec.py
@@ -1,7 +1,15 @@
 import numpy as np
 import IPython
 from ...constants import SMALL_RADIUS, VERTEX
+from ..constants import EdgeE
+from ...math import createLine, bezier1cp, bezier2cp, get_distance
 from ...drawing import drawRect, drawCircle, clear_canvas, drawText
+from .. import Face, HalfEdge, Vertex
+import cairo_utils.easings as easings
+
+import logging as root_logger
+logging = root_logger.getLogger(__name__)
+
 
 class SampleSpec:
     """ Abstract Spec to describe the sampling transform 
@@ -35,8 +43,63 @@ class SampleSpec:
 
     def primary_sample(self, target, data):
         """ The method to override """
-        raise Exception("SampleSpec.primary_sample should be overriden")
+        if isinstance(target, Vertex):
+            return self.primary_vertex(target, data)
+        elif isinstance(target, HalfEdge):
+            return self.primary_halfEdge(target, data)
+        elif isinstance(target, Face):
+            return self.primary_face(target,data)
+        else:
+            raise Exception("TODO: Step Down Automatically on Missing Sample Functions")
+        
+        
+    def primary_vertex(self, target, data):
+        """ Samples a single vertex """
+        return np.array([target.toArray()])
+        
 
+    def primary_halfEdge(self, target, data):
+        """ Sample the halfEdge to start with """
+        assert("sample_amnt" in data)
+        #Primary
+        sample_amount = data["sample_amnt"]
+        pxys = np.zeros((1,2))
+    
+        #calculate the actual line
+        logging.info("Sampling a line")
+        #start and end
+        if EdgeE.BEZIER in target.data:
+            for b in target.data[EdgeE.BEZIER]:
+                num_points = get_distance(b[0],b[-1]) * sample_amount
+                if len(b) == 3:
+                    new_points = bezier1cp(*b, num_points)
+                else:
+                    assert(len(b) == 4)
+                    new_points = bezier2cp(*b, num_points)
+                pxys = np.row_stack((pxys, new_points))
+        else:
+            #straight line
+            num_points = get_distance(se[0], se[1]) * sample_amount
+            pxys = np.row_stack((pxys, createLine(*se[0], *se[1], num_points)))
+        assert(pxys is not None)
+        return pxys[1:]
+
+
+    def primary_face(self, target, data):
+        """ Samples a single Face """
+        #create sample amnt of points within the bounds of the face
+        if "fill" in data:
+            #choose points from within the polygon
+            return np.array([])
+        elif "stroke" in data:
+            #choose points along the edges
+            return np.array([])
+        elif "corner" in data:
+            #choose points at the corners of the polygon
+            return np.array([])
+        else:
+            raise Exception("Unrecognised face sample instruction")
+    
     def secondary_sample(self, core, target, data):
         """ The method to override """
         raise Exception("SampleSpec.secondary_sample should be overriden")
@@ -48,6 +111,31 @@ class SampleSpec:
             ctx.set_source_rgba(*line[3:])
             drawCircle(ctx, *line[:2], line[2])
 
+    def getEasing(self, key, data):
+        try:
+            easingData = data[key]
+            easing_fn = easings.lookup(easingData[0])
+            if len(easingData) > 2:
+                codomain = easingData[2]
+            else:
+                codomain = easings.CODOMAIN.FULL
+            if len(easingData) > 3:
+                quantize = easingData[3]
+            else:
+                quantize = 0
+
+            if quantize != 0:
+                easing_lambda = lambda xs: easings.quantize(easing_fn(xs, easingData[1], codomain_e=codomain), q=quantize)
+            else:
+                easing_lambda = lambda xs: easing_fn(xs, easingData[1], codomain_e=codomain)
+            return (easing_lambda)
+        except Exception as e:
+            logging.warning(e)
+            easing_fn = easings.lookup(easings.ENAMES[0])
+            return (lambda xs: easing_fn(xs, 1, codomain_e=easings.CODOMAIN.FULL))
+
+        
+            
 
 def defaultPost(core, secondary, target, data):
     """ add default radius and colours to points """
--- cairo_utils/dcel/sample_specs/VectorSample.py
+++ cairo_utils/dcel/sample_specs/VectorSample.py
@@ -2,13 +2,12 @@ import numpy as np
 from ..constants import EdgeE
 from ...math import createLine, bezier1cp, bezier2cp
 from .SampleSpec import SampleSpec
+from .. import Face, Vertex, HalfEdge
 import cairo_utils.easings as easings
 
 import logging as root_logger
 import IPython
 logging = root_logger.getLogger(__name__)
-
-
 
 class VectorSample(SampleSpec):
     """ An implementation of a sample spec to translate by a vector """
@@ -17,42 +16,20 @@ class VectorSample(SampleSpec):
         super().__init__(data, postFunction)
         #setup default values
 
-        
-    def primary_sample(self, target, data):
-        """ The method to override """
+    def secondary_sample(self, core, target, data):
+        #for the xys, sample each vec_amnt times, along the vector, to distance
         assert("vector" in data)
         assert("distance" in data)
         assert("sample_amnt" in data)
         assert("vec_amnt" in data)
-        #Primary
-        pxys = None
-        sample_amount = data["sample_amnt"]
-        vec_array = np.repeat(data["vector"], data["vec_amnt"]).reshape((2,-1))
-        vec_array *= data["distance"]
-        vec_array = vec_array.T
-    
-        #calculate the actual line
-        logging.info("Sampling a line")
-        #start and end
-        se = target.toArray()
-        if EdgeE.BEZIER in target.data:
-            #get control points, calculate
-            cp1, cp2 = target.data[EdgeE.BEZIER]
-            if cp2 is None:
-                pxys = bezier1cp(se[0], cp1, se[1], sample_amount)
-            else:
-                pxys = bezier2cp(se[0], cp1, cp2, se[1], sample_amount)
-        else:
-            #straight line
-            pxys = createLine(*se[0], *se[1], sample_amount)
-        assert(pxys is not None)
-        return pxys
 
-    def secondary_sample(self, core, target, data):
-        #for the xys, sample each vec_amnt times, along the vector, to distance
+        easing_1_fn = self.getEasing("easing_1", data)
+        easing_1 = easing_1_fn(np.random.random(data["vec_amnt"]))
+
+
         vec_array = np.repeat(data["vector"], data["vec_amnt"]).reshape((2,-1))
         vec_array *= data["distance"]
-        vec_array *= easings.pow_abs_curve(np.linspace(0,1,data["vec_amnt"]), 1)
+        vec_array *= easing_1
         vec_array = vec_array.T
 
         sxys = np.zeros((1,data["vec_amnt"],2))
