c4c81643a40ab61a78837f86aa456bada449ec9d
John Grey <jgrey@ucsc.edu>
2018-07-01 07:58:30 +0100

    added a description to heapWrapper, and added a check to pop_while_same

--- cairo_utils/heaputils.py
+++ cairo_utils/heaputils.py
@@ -8,9 +8,10 @@ logging = root_logger.getLogger(__name__)
 
 class HeapWrapper:
     """ Utility to wrap an ordinal with data to use in the heap """
-    def __init__(self, ord, data):
+    def __init__(self, ord, data, desc=None):
         self.ord = ord
         self.data = data
+        self.desc = desc
 
     def __lt__(self, other):
         assert(isinstance(other, HeapWrapper))
@@ -20,7 +21,10 @@ class HeapWrapper:
         return (self.ord, self.data)
 
     def __repr__(self):
-        return "{} - {}".format(self.ord, repr(self.data))
+        if self.desc is None:
+            return "{} - {}".format(self.ord, repr(self.data))
+        else:
+            return "{} - {} : {}".format(self.ord, self.desc, repr(self.data))
 
 def pop_while_same(heap):
     """ Pop while the head is equal to the first value poppped """
@@ -30,10 +34,12 @@ def pop_while_same(heap):
         return (first_vert, [])
     
     collected = (first_vert, [first_edge])
+    count = 1
     while bool(heap) and heap[0].ord == first_vert:
         data = heapq.heappop(heap).data
         if data is not None:
             collected[1].append(data)
-
+            count += 1
+    assert(len(collected[1]) == count)
     return collected
 
