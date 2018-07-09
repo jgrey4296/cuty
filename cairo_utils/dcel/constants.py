685f697e312af358cf1eb5674fcc0861fda3d9db
John Grey <jgrey@ucsc.edu>
2018-07-09 21:17:16 +0100

    improved sweep_nudge definition

--- cairo_utils/dcel/constants.py
+++ cairo_utils/dcel/constants.py
@@ -16,4 +16,4 @@ EDGE_FOLLOW_GUARD = 200
 #The amount used to nudge forwards/down the sweep line in the y direction
 #for line segment intersection in dcel.intersect_halfedges
 #Default: -0.1 for cartesian bboxs of larger than 0-1
-SWEEP_NUDGE = -0.1
+SWEEP_NUDGE = - 1e-3
