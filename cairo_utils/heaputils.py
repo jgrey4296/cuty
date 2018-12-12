4a17ac4e6e1ea202d44d0be48c74a52cb2c47d6a
John Grey <jgrey@ucsc.edu>
2018-12-12 19:31:33 +0000

    bringing heaputils to pass pylint

--- cairo_utils/heaputils.py
+++ cairo_utils/heaputils.py
@@ -3,28 +3,28 @@ Utilities for using heapq
 """
 import heapq
 import logging as root_logger
-import IPython
 logging = root_logger.getLogger(__name__)
 
 class HeapWrapper:
     """ Utility to wrap an ordinal with data to use in the heap """
-    def __init__(self, ord, data, desc=None):
-        self.ord = ord
+    def __init__(self, ordinal, data, desc=None):
+        self.ordinal = ordinal
         self.data = data
         self.desc = desc
 
     def __lt__(self, other):
         assert(isinstance(other, HeapWrapper))
-        return self.ord < other.ord
+        return self.ordinal < other.ordinal
 
     def unwrap(self):
-        return (self.ord, self.data)
+        """ Unwrap the data """
+        return (self.ordinal, self.data)
 
     def __repr__(self):
         if self.desc is None:
-            return "{} - {}".format(self.ord, repr(self.data))
+            return "{} - {}".format(self.ordinal, repr(self.data))
         else:
-            return "{} - {} : {}".format(self.ord, self.desc, repr(self.data))
+            return "{} - {} : {}".format(self.ordinal, self.desc, repr(self.data))
 
 def pop_while_same(heap):
     """ Pop while the head is equal to the first value poppped """
@@ -32,7 +32,7 @@ def pop_while_same(heap):
     first_vert, first_edge = heapq.heappop(heap).unwrap()
     if first_edge is None:
         return (first_vert, [])
-    
+
     collected = (first_vert, [first_edge])
     count = 1
     while bool(heap) and heap[0].ord == first_vert:
@@ -42,4 +42,3 @@ def pop_while_same(heap):
             count += 1
     assert(len(collected[1]) == count)
     return collected
-
