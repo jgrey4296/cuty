a50cf2dbc475fb2a5e47f2a55aae01d2d433ded3
John Grey <jgrey@ucsc.edu>
2018-12-12 19:20:50 +0000

    bringing quadratic to pass pylint

--- cairo_utils/Quadratic.py
+++ cairo_utils/Quadratic.py
@@ -1,57 +1,64 @@
-from math import sqrt, trunc
+"""
+Provides a Quadratic Clss, allowing manipulation of quadratic equations
+"""
+import logging as root_logger
+from math import sqrt
 import numpy as np
-import logging
-import IPython
 
-class Quadratic(object):
-    """ A Class to hold a quadratic equation 
+logging = root_logger.getLogger(__name__)
+
+class Quadratic:
+    """ A Class to hold a quadratic equation
     (y = ax^2 + bx + c)
     and perform operations on it """
-    
-    def __init__(self,a,b,c):
+
+    #pylint: disable=invalid-name
+    def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+    #pylint: enable=invalid-name
 
-    def __call__(self,x):
+    def __call__(self, x):
         """ Shorthand to get y for a given x """
         if x is None:
             return None
-        result = (self.a * pow(x,2)) + (self.b * x) + self.c
+        result = (self.a * pow(x, 2)) + (self.b * x) + self.c
         return result
 
-    def intersect(self,q2):
+    def intersect(self, q2):
         """ Get the x coordinates of the intersections of the two quadratics """
         assert(isinstance(q2, Quadratic))
         aprime = q2.a - self.a
         bprime = q2.b - self.b
         cprime = q2.c - self.c
-        
-        q3 = Quadratic(aprime,bprime,cprime)
-        
+
+        q3 = Quadratic(aprime, bprime, cprime)
+
         xs = q3.solve()
         xs_existing = np.array([x for x in xs if x is not None])
         xs_existing.sort()
         return xs_existing
 
     def discriminant(self):
-        return pow(self.b,2) - (4 * self.a * self.c)
+        """ Get the discriminant of the quadratic """
+        return pow(self.b, 2) - (4 * self.a * self.c)
 
     def solve(self):
         """ Solve the quadratic, returning up to two values"""
-        returnVal = []
-        D = self.discriminant()
+        return_value = []
+        discriminant = self.discriminant()
         numerator_a = -self.b
         denominator = 2 * self.a
-        if D < 0:
-            returnVal = [None,None]
-        elif np.allclose(D,0) or np.allclose(self.a,0):
+        if discriminant < 0:
+            return_value = [None, None]
+        elif np.allclose(discriminant, 0) or np.allclose(self.a, 0):
             logging.debug('Only one intersection')
             #using mullers method:
             twoc = -2 * self.c
-            sqrtD = sqrt(D)
-            pos = (-self.b) + sqrtD
-            neg = (-self.b) - sqrtD
+            sqrt_discriminant = sqrt(discriminant)
+            pos = (-self.b) + sqrt_discriminant
+            neg = (-self.b) - sqrt_discriminant
             if pos != 0:
                 x = twoc / pos
             elif neg != 0:
@@ -59,19 +66,18 @@ class Quadratic(object):
             else:
                 logging.debug("Not even one intersection")
                 x = None
-            returnVal = [x,None]
+            return_value = [x, None]
         else:
-            z = sqrt(D)
-            returnVal = [
+            z = sqrt(discriminant)
+            return_value = [
                 (numerator_a + z) / denominator,
                 (numerator_a - z) / denominator,
             ]
-        return returnVal
+        return return_value
 
     # def solve(self):
     #     twoA = 2 * self.a
-    #     sqrtb4ac = sqrt(pow(self.b,2) - (4 * self.a * self.c))
+    #     sqrtb4ac = sqrt(pow(self.b, 2) - (4 * self.a * self.c))
     #     pos = -self.b + sqrtb4ac
     #     neg = -self.b - sqrtb4ac
     #     return [neg, pos]
-
