03889759da910d59173bc94a808bf2c30851ef93
John Grey <jgrey@ucsc.edu>
2018-12-12 21:47:57 +0000

    bring rbtree operations to pass pylint

--- cairo_utils/rbtree/__init__.py
+++ cairo_utils/rbtree/__init__.py
@@ -1,11 +1,12 @@
-### A Red-Black Tree Implementation
-# Properties of RBTrees:
-# 1) Every node is Red Or Black
-# 2) The root is black
-# 3) Every leaf is Black, leaves are null nodes
-# 4) If a node is red, it's children are black
-# 5) All paths from a node to its leaves contain the same number of black nodes
-
+"""
+A Red-Black Tree Implementation
+Properties of RBTrees:
+1) Every node is Red Or Black
+2) The root is black
+3) Every leaf is Black, leaves are null nodes
+4) If a node is red, it's children are black
+5) All paths from a node to its leaves contain the same number of black nodes
+"""
 from .RBTree import RBTree
 from .Node import Node
-from .ComparisonFunctions import Directions
+from .comparison_functions import Directions
--- cairo_utils/rbtree/operations.py
+++ cairo_utils/rbtree/operations.py
@@ -1,77 +1,77 @@
+""""
+Red Black Tree Operations, as translated from Cormen.
+todo: integrate these into the beachline class
+"""
 import logging as root_logger
 from .Node import Node
-from .ComparisonFunctions import Directions
-import IPython
 
 logging = root_logger.getLogger(__name__)
 
-""" Below are the functional implementations of Red-Black Tree operations """
-#todo: integrate these into the beachline class 
-def rotateLeft(tree,node):
+def rotate_left(tree, node):
     """ Rotate the given node left, making the new head be node.right """
     logging.debug("Rotating Left: {}".format(node))
-    assert(isinstance(node,Node))
+    assert(isinstance(node, Node))
     assert(node.right is not None)
-    setAsRoot, newHead = node.rotate_left()
-    if setAsRoot:
-        tree.root = newHead
-    return newHead
+    set_as_root, new_head = node.rotate_left()
+    if set_as_root:
+        tree.root = new_head
+    return new_head
 
-def rotateRight(tree,node):
+def rotate_right(tree, node):
     """ Rotate the given node right, making the new head be node.left """
     logging.debug("Rotating Right: {}".format(node))
     assert(isinstance(node, Node))
     assert(node.left is not None)
-    setAsRoot, newHead = node.rotate_right()
-    if setAsRoot:
-        tree.root = newHead
-    return newHead
-        
-def rbtreeFixup(tree,node):
+    set_as_root, new_head = node.rotate_right()
+    if set_as_root:
+        tree.root = new_head
+    return new_head
+
+def rb_tree_fixup(tree, node):
     """ Verify and fix the RB properties hold """
-    while node.parent != None and node.parent.red:
+    while node.parent is not None and node.parent.red:
         parent = node.parent
-        grandParent = parent.parent
-        if grandParent == None:
+        grand_parent = parent.parent
+        if grand_parent is None:
             break
-        elif parent == grandParent.left:
-            y = grandParent.right
-            if y != None and y.red:
+        elif parent == grand_parent.left:
+            y = grand_parent.right
+            if y is not None and y.red:
                 parent.red = False
                 y.red = False
-                grandParent.red = True
-                node = grandParent
+                grand_parent.red = True
+                node = grand_parent
             else:
                 if node == parent.right:
                     node = parent
-                    rotateLeft(tree,node)#invalidates parent and grandparent
+                    rotate_left(tree, node)#invalidates parent and grandparent
                 node.parent.red = False
                 node.parent.parent.red = True
-                rotateRight(tree,node.parent.parent)
+                rotate_right(tree, node.parent.parent)
         else:
-            y = grandParent.left
-            if y != None and y.red:
+            y = grand_parent.left
+            if y is not None and y.red:
                 parent.red = False
                 y.red = False
-                grandParent.red = True
-                node = grandParent
+                grand_parent.red = True
+                node = grand_parent
             else:
                 if node == parent.left:
                     node = parent
-                    rotateRight(tree,node)#invalidates parent and grandparent
+                    rotate_right(tree, node)#invalidates parent and grandparent
                 node.parent.red = False
                 node.parent.parent.red = True
-                rotateLeft(tree,node.parent.parent)
+                rotate_left(tree, node.parent.parent)
     tree.root.red = False
 
-def transplant(tree,target,replacement):
-    """ Transplant the node replacement, and its subtree, 
+def transplant(tree, target, replacement):
+    """ Transplant the node replacement, and its subtree,
     in place of node target """
-    #logging.debug("Transplanting {} into {}".format(replacement,target))
+    #logging.debug("Transplanting {} into {}".format(replacement, target))
     if replacement is not None:
         replacement.disconnect_from_parent()
 
-    if target.parent == None:
+    if target.parent is None:
         logging.debug("Setting root to {}".format(replacement))
         tree.root = replacement
     elif target.parent.on_left(target):
@@ -86,50 +86,53 @@ def transplant(tree,target,replacement):
         parent.link_right(replacement)
 
 
-def rbTreeDelete(tree,node):
-    target = node 
+def rb_tree_delete(tree, node):
+    """ Delete a node from the tree and rotate nodes as needed """
+    target = node
     target_originally_red = target.red
     current = None
     logging.debug("Deleting Node: {}".format(node))
     if target.left is None:
         logging.debug("No left, transplanting right")
         current = target.right
-        transplant(tree,target,target.right)
+        transplant(tree, target, target.right)
     elif target.right is None:
         logging.debug("No right, transplanting left")
         current = target.left
-        transplant(tree,target,target.left)
+        transplant(tree, target, target.left)
     else:
         logging.debug("Both Left and right exist")
         target = target.right.min()
         target_originally_red = target.red
         current = target.right
         if target.parent == node:
-            if current != None:
+            if current is not None:
                 logging.debug("target.parent == node")
                 target.disconnect_from_parent()
                 current.parent = target
         else:
             logging.debug("target.parent != node")
-            transplant(tree,target,target.right)
+            transplant(tree, target, target.right)
             target.link_right(node.right)
 
-        transplant(tree,node,target)
+        transplant(tree, node, target)
         target.link_left(node.left)
         target.red = node.red
-        
+
     if not target_originally_red:
         logging.debug("Fixing up current: {}".format(current))
-        rbDeleteFixup(tree,current)
-    
-def rbDeleteFixup(tree,node):
+        rb_delete_fixup(tree, current)
+
+def rb_delete_fixup(tree, node):
+    """ Having deleted a node, rotate remaining nodes
+    as necessary to preserve rb properties """
     while node != tree.root and node is not None and not node.red:
         if node.parent.on_left(node):
             w = node.parent.right
             if w is not None and w.red:
                 w.red = False
                 node.parent.red = True
-                rotateLeft(tree,node.parent)
+                rotate_left(tree, node.parent)
                 w = node.parent.right
             if w is not None and (w.left is None or not w.left.red) and \
                (w.right is None or not w.right.red):
@@ -139,19 +142,19 @@ def rbDeleteFixup(tree,node):
                 if w.right is None or not w.right.red:
                     w.left.red = False
                     w.red = True
-                    rotateRight(tree,w)
+                    rotate_right(tree, w)
                     w = node.parent.right
                 w.red = node.parent.red
                 node.parent.red = False
                 w.right.red = False
-                rotateLeft(tree,node.parent)
+                rotate_left(tree, node.parent)
                 node = tree.root
         else: #mirror for right
             w = node.parent.left
             if w is not None and w.red:
                 w.red = False
                 node.parent.red = True
-                rotateRight(tree,node.parent)
+                rotate_right(tree, node.parent)
                 w = node.parent.left
             if w is not None and (w.left is None or not w.left.red) and \
                (w.right is None or not w.right.red):
@@ -161,16 +164,13 @@ def rbDeleteFixup(tree,node):
                 if w.left is None or not w.left.red:
                     w.right.red = False
                     w.red = True
-                    rotateLeft(tree,w)
+                    rotate_left(tree, w)
                     w = node.parent.left
                 w.red = node.parent.red
                 node.parent.red = False
                 w.left.red = False
-                rotateRight(tree,node.parent)
+                rotate_right(tree, node.parent)
                 node = tree.root
         node.red = False
         if w is None:
             node = None
- 
-
-
