8c17da146ab7a764cecf7fee6948bfc73b57f4ee
John Grey <jgrey@ucsc.edu>
2018-12-12 15:38:34 +0000

    updated drawing to pass pylint

--- cairo_utils/drawing.py
+++ cairo_utils/drawing.py
@@ -1,43 +1,73 @@
-import cairo
+"""
+Drawing: provides basic setup and drawing utilities for cairo:
+"""
 import logging as root_logger
-from .constants import TEXT, BACKGROUND, TWOPI, FRONT, FONT_SIZE
-import random
+import cairo
+from .constants import BACKGROUND, TWOPI, FRONT, FONT_SIZE
 
 logging = root_logger.getLogger(__name__)
 
-def setup_cairo(N=5, font_size=FONT_SIZE, scale=True, cartesian=False, background=BACKGROUND):
-    size = pow(2, N)
+def setup_cairo(n=5, font_size=FONT_SIZE, scale=True, cartesian=False, background=BACKGROUND):
+    """
+    Utility a Cairo surface and context
+    n : the pow2 size of the surface
+    font_size
+    scale : True for coords of -1 to 1
+    cartesian : True for (0,0) being in the bottom left
+    background : The background colour to initialize to
+    """
+    size = pow(2, n)
     surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, size, size)
     ctx = cairo.Context(surface)
     if cartesian:
-        ctx.scale(1,-1)
-        ctx.translate(0,-size)        
+        ctx.scale(1, -1)
+        ctx.translate(0, -size)
     if scale:
         ctx.scale(size, size)
     ctx.set_font_size(font_size)
     clear_canvas(ctx, colour=background)
-    return (surface, ctx, size, N)
+    return (surface, ctx, size, n)
 
 
 def write_to_png(surface, filename, i=None):
+    """ Write the given surface to a png, with optional numeric postfix
+    surface : The surface to write
+    filename : Does not need file type postfix
+    i : optional numeric
+    """
     if i:
         surface.write_to_png("{}_{}.png".format(filename, i))
     else:
         surface.write_to_png("{}.png".format(filename))
 
-def drawRect(ctx, x, y, sx, sy):
+def draw_rect(ctx, xyxys, fill=True):
+    """ Draw simple rectangles.
+    Takes the context and a (n,4) array
+    """
     #ctx.set_source_rgba(*FRONT)
-    ctx.rectangle(x, y, sx, sy)
-    ctx.fill()
-    
-def drawCircle(ctx, x, y, r, fill=True):
-    ctx.arc(x, y, r, 0, TWOPI)
+    for a in xyxys:
+        ctx.rectangle(*a)
+    if fill:
+        ctx.fill()
+    else:
+        ctx.stroke()
+
+def draw_circle(ctx, xyrs, fill=True):
+    """ Draw simple circles
+    Takes context,
+    """
+    for a in xyrs:
+        ctx.arc(*a, 0, TWOPI)
     if fill:
         ctx.fill()
     else:
         ctx.stroke()
 
 def clear_canvas(ctx, colour=BACKGROUND, bbox=None):
+    """ Clear a rectangle of a context using particular colour
+    colour : The colour to clear to
+    bbox : The area to clear, defaults to (0,0,1,1)
+    """
     ctx.set_source_rgba(*colour)
     if bbox is None:
         ctx.rectangle(0, 0, 1, 1)
@@ -46,17 +76,14 @@ def clear_canvas(ctx, colour=BACKGROUND, bbox=None):
     ctx.fill()
     ctx.set_source_rgba(*FRONT)
 
-def drawText(ctx, xy, text, offset=False):
+def draw_text(ctx, xy, text):
+    """ Utility to simplify drawing text
+    Takes context, position, text
+    """
     logging.debug("Drawing text: {}, {}".format(text, xy))
     ctx.save()
-    if offset:
-        offset = random.random() * 0.005
-    else:
-        offset = 0
-    xy += offset
-    ctx.set_source_rgba(*TEXT)
     ctx.move_to(*xy)
-    ctx.scale(1,-1)
+    ctx.scale(1, -1)
     ctx.show_text(str(text))
-    ctx.scale(1,-1)
+    ctx.scale(1, -1)
     ctx.restore()
