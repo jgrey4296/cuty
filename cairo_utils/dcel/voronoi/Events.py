8460fd45e9190d5323e15041fc5c94604cf8d6ff
John Grey <jgrey@ucsc.edu>
2018-12-13 20:57:37 +0000

    brings voronoi events to pass pylint

--- cairo_utils/dcel/voronoi/Events.py
+++ cairo_utils/dcel/voronoi/Events.py
@@ -1,45 +1,51 @@
-""" Events: The data representations of points and circles 
+""" Events: The data representations of points and circles
     in the voronoi calculation
 """
-import IPython
-import numpy as np
 from enum import Enum
+import numpy as np
+
 
 CIRCLE_EVENTS = Enum("Circle Event Sides", "LEFT RIGHT")
 
 class VEvent:
+    """ The Base Class of events in fortunes algorithm """
     offset = 0
-    
+
     """ The Base Class of Events """
-    def __init__(self,site_location,i=-1):
+    def __init__(self, site_location, i=-1):
         assert(isinstance(site_location, np.ndarray))
         self.loc = site_location #tuple
         self.step = i
 
     def y(self):
+        """ Get the vertical position of the event """
         return self.loc[1]
 
-    def __lt__(self,other):
+    def __lt__(self, other):
         return (VEvent.offset - self.y()) < (VEvent.offset - other.y())
-    
+
 class SiteEvent(VEvent):
     """ Subclass for representing individual points / cell centres """
-    def __init__(self,site_loc,i=None,face=None):
-        super().__init__(site_loc,i=i)
+    def __init__(self, site_loc, i=None, face=None):
+        super().__init__(site_loc, i=i)
         self.face = face
-        
+
     def __str__(self):
         return "Site Event: Loc: {}".format(self.loc)
 
 class CircleEvent(VEvent):
-    """ Subclass for representing the lowest point of a circle, 
+    """ Subclass for representing the lowest point of a circle,
     calculated from three existing site events """
-    def __init__(self,site_loc,sourceNode,voronoiVertex,left=True,i=None):
-        if left and (CIRCLE_EVENTS.RIGHT in sourceNode.data and sourceNode.data[CIRCLE_EVENTS.RIGHT].active):
-            raise Exception("Trying to add a circle event to a taken left node: {} : {}".format(sourceNode,sourceNode.data[CIRCLE_EVENTS.RIGHT]))
-        elif not left and (CIRCLE_EVENTS.LEFT in sourceNode.data and sourceNode.data[CIRCLE_EVENTS.LEFT].active):
-            raise Exception("Trying to add a circle event to a taken right node: {} : {}".format(sourceNode,sourceNode.data[CIRCLE_EVENTS.LEFT]))
-        super().__init__(site_loc,i=i)
+    def __init__(self, site_loc, sourceNode, voronoiVertex, left=True, i=None):
+        if left and (CIRCLE_EVENTS.RIGHT in sourceNode.data
+                     and sourceNode.data[CIRCLE_EVENTS.RIGHT].active):
+            exception_text = "Trying to add a circle event to a taken left node: {} : {}"
+            raise Exception(exception_text.format(sourceNode, sourceNode.data[CIRCLE_EVENTS.RIGHT]))
+        elif not left and (CIRCLE_EVENTS.LEFT in sourceNode.data
+                           and sourceNode.data[CIRCLE_EVENTS.LEFT].active):
+            exception_text = "Trying to add a circle event to a taken right node: {} : {}"
+            raise Exception(exception_text.format(sourceNode, sourceNode.data[CIRCLE_EVENTS.LEFT]))
+        super().__init__(site_loc, i=i)
         #The node that will disappear
         self.source = sourceNode
         #the breakpoint where it will disappear
@@ -51,16 +57,13 @@ class CircleEvent(VEvent):
             sourceNode.data[CIRCLE_EVENTS.RIGHT] = self
         else:
             sourceNode.data[CIRCLE_EVENTS.LEFT] = self
-            
+
     def __str__(self):
         return "Circle Event: {}, Node: {}, Left: {}, Added On Step: {}".format(self.loc,
                                                                                 self.source,
                                                                                 self.left,
                                                                                 self.step)
-            
+
     def deactivate(self):
         """ Deactivating saves on having to reheapify """
         self.active = False
-
-
-
--- cairo_utils/dcel/voronoi/__init__.py
+++ cairo_utils/dcel/voronoi/__init__.py
@@ -1,1 +1,4 @@
+"""
+Voronoi implements Fortunes Algorithm for the DCEL
+"""
 from .voronoi import Voronoi
