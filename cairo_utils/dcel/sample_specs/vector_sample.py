96d0cf91199b3e52913c4de3cd579aae4a8b693b
John Grey <jgrey@ucsc.edu>
2018-12-13 19:42:43 +0000

    brings vector_sample to pass pylint

--- cairo_utils/dcel/sample_specs/vector_sample.py
+++ cairo_utils/dcel/sample_specs/vector_sample.py
@@ -1,41 +1,39 @@
-import numpy as np
-from ..constants import EdgeE
-from ...math import createLine, bezier1cp, bezier2cp
-from .SampleSpec import SampleSpec
-from .. import Face, Vertex, HalfEdge
-import cairo_utils.easings as easings
-
+"""
+Provides a sampler that translates by a fixed vector
+"""
 import logging as root_logger
-import IPython
+import numpy as np
+from .sample_spec import SampleSpec
+
 logging = root_logger.getLogger(__name__)
 
 class VectorSample(SampleSpec):
     """ An implementation of a sample spec to translate by a vector """
 
-    def __init__(self, data, postFunction=None):
-        super().__init__(data, postFunction)
+    def __init__(self, data, post_function=None):
+        super().__init__(data, post_function)
         #setup default values
 
     def secondary_sample(self, core, target, data):
-        #for the xys, sample each vec_amnt times, along the vector, to distance
+        """ for the xys, sample each vec_amnt times,
+        along the vector, to distance
+        """
         assert("vector" in data)
         assert("distance" in data)
         assert("sample_amnt" in data)
         assert("vec_amnt" in data)
 
-        easing_1_fn = self.getEasing("easing_1", data)
+        easing_1_fn = self.get_easing("easing_1", data)
         easing_1 = easing_1_fn(np.random.random(data["vec_amnt"]))
 
-
-        vec_array = np.repeat(data["vector"], data["vec_amnt"]).reshape((2,-1))
+        vec_array = np.repeat(data["vector"], data["vec_amnt"]).reshape((2, -1))
         vec_array *= data["distance"]
         vec_array *= easing_1
         vec_array = vec_array.T
 
-        sxys = np.zeros((1,data["vec_amnt"],2))
+        sxys = np.zeros((1, data["vec_amnt"], 2))
         for a in core:
             sxys = np.row_stack((sxys, np.array([a + vec_array])))
 
         #primary, secondary
         return sxys[1:]
-
