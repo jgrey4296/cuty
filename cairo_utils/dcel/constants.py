ccbd065ad9242d0b0f9cb4dbdce42561ce9ff8f5
John Grey <jgrey@ucsc.edu>
2018-12-13 07:54:37 +0000

    brings dcel/dcel_drawing to pass pylint

--- cairo_utils/dcel/constants.py
+++ cairo_utils/dcel/constants.py
@@ -18,3 +18,8 @@ EDGE_FOLLOW_GUARD = 200
 #for line segment intersection in dcel.intersect_halfedges
 #Default: -0.1 for cartesian bboxs of larger than 0-1
 SWEEP_NUDGE = - 1e-3
+
+FACE_COLOUR=[0.2, 0.2, 0.9, 1]
+EDGE_COLOUR=[0.4, 0.8, 0.1, 1]
+VERT_COLOUR=[0.9, 0.1, 0.1, 1]
+BACKGROUND_COLOUR=[0, 0, 0, 1]
--- cairo_utils/dcel/dcel_drawing.py
+++ cairo_utils/dcel/dcel_drawing.py
@@ -1,25 +1,31 @@
+"""
+Provides broad drawing support for a dcel
+"""
+#pylint: disable=too-many-arguments
 import logging as root_logger
-import numpy as np
-from ..constants import START, END, SMALL_RADIUS, FACE, EDGE, VERTEX, WIDTH
-from ..drawing import drawRect, drawCircle, clear_canvas, drawText
-from .Face import Face
-from .dcel import DCEL
-from ..math import get_midpoint
-from .constants import FaceE, EdgeE, VertE
+from ..constants import WIDTH
+from ..drawing import clear_canvas
+from .constants import FACE_COLOUR, EDGE_COLOUR, VERT_COLOUR, BACKGROUND_COLOUR
 
 
 logging = root_logger.getLogger(__name__)
 
-def drawDCEL(ctx, dcel, text=False, faces=True, edges=False, verts=False,
-             face_colour=[0.2, 0.2, 0.9, 1],
-             edge_colour=[0.4, 0.8, 0.1, 1],
-             vert_colour=[0.9, 0.1, 0.1, 1],
-             background_colour=[0,0,0,1],
-             edge_width=WIDTH):
 
+def draw_dcel(ctx, dcel, text=False, faces=True, edges=False, verts=False,
+              face_colour=None, edge_colour=None, vert_colour=None,
+              background_colour=None, edge_width=WIDTH):
     """ A top level function to draw a dcel  """
+    if face_colour is None:
+        face_colour = FACE_COLOUR
+    if edge_colour is None:
+        edge_colour = EDGE_COLOUR
+    if vert_colour is None:
+        vert_colour = VERT_COLOUR
+    if background_colour is None:
+        background_colour = BACKGROUND_COLOUR
+
     clear_canvas(ctx, colour=background_colour, bbox=dcel.bbox)
-    
+
     if faces:
         ctx.set_source_rgba(*face_colour)
         draw_dcel_faces(ctx, dcel, text=text)
@@ -33,24 +39,24 @@ def drawDCEL(ctx, dcel, text=False, faces=True, edges=False, verts=False,
         draw_dcel_vertices(ctx, dcel)
 
 def draw_dcel_faces(ctx, dcel, text=True, clear=False):
+    """ Draw ll faces in a dcel """
     for f in dcel.faces:
-        sample_data = f.draw(ctx, clear=clear, text=text)
+        f.draw(ctx, clear=clear, text=text)
 
 def draw_dcel_edges(ctx, dcel, text=True, width=WIDTH):
-    originalTextState = text
-    drawnTexts = set()
+    """ Draw all edges in a dcel """
+    original_text_state = text
+    drawn_texts = set()
     for edge in dcel.halfEdges:
-        if edge.index not in drawnTexts:
-            text = originalTextState
-            drawnTexts.add(edge.index)
-            drawnTexts.add(edge.twin.index)
+        if edge.index not in drawn_texts:
+            text = original_text_state
+            drawn_texts.add(edge.index)
+            drawn_texts.add(edge.twin.index)
         else:
             text = False
         edge.draw(ctx, text=text, width=width)
-        
+
 def draw_dcel_vertices(ctx, dcel):
     """ Draw all the vertices in a dcel as dots """
     for v in dcel.vertices:
-        sample_data = v.draw(ctx)
-        
-        
+        v.draw(ctx)
