440eaad85e511d5a40507a243b06db947cdbdea9
John Grey <jgrey@ucsc.edu>
2018-12-13 19:46:59 +0000

    brought circle_sample to pass pylint

--- cairo_utils/dcel/sample_specs/circle_sample.py
+++ cairo_utils/dcel/sample_specs/circle_sample.py
@@ -1,11 +1,11 @@
-import numpy as np
-from ..constants import EdgeE
-from ...math import createLine, bezier1cp, bezier2cp, randomRad, rotMatrix
-from .SampleSpec import SampleSpec
-import cairo_utils.easings as easings
-
+"""
+Provides a sampler operating in a random circle
+"""
 import logging as root_logger
-import IPython
+import numpy as np
+from ...math import random_radian, rotation_matrix
+from .sample_spec import SampleSpec
+
 logging = root_logger.getLogger(__name__)
 
 class CircleSample(SampleSpec):
@@ -20,23 +20,23 @@ class CircleSample(SampleSpec):
         #for the xys, sample each vec_amnt times, along the vector, to distance
         assert("distance" in data)
         assert("vec_amnt" in data)
-        sxys = np.zeros((1,data["vec_amnt"],2))
+        sxys = np.zeros((1, data["vec_amnt"], 2))
         if data["vec_amnt"] == 0:
             return sxys[1:]
-        
-        easing_1_fn = self.getEasing("easing_1", data)
-        easing_2_fn = self.getEasing("easing_2", data)
+
+        easing_1_fn = self.get_easing("easing_1", data)
+        easing_2_fn = self.get_easing("easing_2", data)
         #distance
         easing_1 = easing_1_fn(np.random.random(data["vec_amnt"]))
         easing_1 *= data["distance"]
-        
-        easing_2 = easing_2_fn(np.linspace(0,1,data["vec_amnt"]))
-        
+
+        easing_2 = easing_2_fn(np.linspace(0, 1, data["vec_amnt"]))
+
         shuffler = "shuffle" in data and data["shuffle"]
 
         dist = easing_1
-        rads = randomRad(shape=(data["vec_amnt"],))
-        vec_array = np.array([np.array([1,0]) @ rotMatrix(x) for x in rads]).T
+        rads = random_radian(shape=(data["vec_amnt"], ))
+        vec_array = np.array([np.array([1, 0]) @ rotation_matrix(x) for x in rads]).T
         vec_array *= dist
         vec_array *= easing_2
         vec_array = vec_array.T
@@ -44,13 +44,12 @@ class CircleSample(SampleSpec):
 
         for a in core:
             if shuffler:
-                rads = randomRad(shape=(data["vec_amnt"],))
-                vec_array = np.array([np.array([1,0]) @ rotMatrix(x) for x in rads]).T
+                rads = random_radian(shape=(data["vec_amnt"], ))
+                vec_array = np.array([np.array([1, 0]) @ rotation_matrix(x) for x in rads]).T
                 vec_array *= dist
                 vec_array *= easing_2
-                vec_array = vec_array.T                
+                vec_array = vec_array.T
             sxys = np.row_stack((sxys, np.array([a + vec_array])))
 
         #primary, secondary
         return sxys[1:]
-
