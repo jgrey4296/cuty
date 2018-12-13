37ed5b7f5d9ce0c509ce1b0a0a6f684b2941d983
John Grey <jgrey@ucsc.edu>
2018-12-13 16:26:50 +0000

    improved formatting of dcel/constants

--- cairo_utils/dcel/constants.py
+++ cairo_utils/dcel/constants.py
@@ -19,7 +19,7 @@ EDGE_FOLLOW_GUARD = 200
 #Default: -0.1 for cartesian bboxs of larger than 0-1
 SWEEP_NUDGE = - 1e-3
 
-FACE_COLOUR=[0.2, 0.2, 0.9, 1]
-EDGE_COLOUR=[0.4, 0.8, 0.1, 1]
-VERT_COLOUR=[0.9, 0.1, 0.1, 1]
-BACKGROUND_COLOUR=[0, 0, 0, 1]
+FACE_COLOUR = [0.2, 0.2, 0.9, 1]
+EDGE_COLOUR = [0.4, 0.8, 0.1, 1]
+VERT_COLOUR = [0.9, 0.1, 0.1, 1]
+BACKGROUND_COLOUR = [0, 0, 0, 1]
