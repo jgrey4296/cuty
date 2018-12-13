a1f213c651d7d660a51ce910992a9faddd886a37
John Grey <jgrey@ucsc.edu>
2018-12-13 17:31:43 +0000

    bring sample_spects/__init__ to pass pylint

--- cairo_utils/dcel/sample_specs/__init__.py
+++ cairo_utils/dcel/sample_specs/__init__.py
@@ -1,5 +1,9 @@
-from .SampleSpec import SampleSpec
-from .VectorSample import VectorSample
-from .CircleSample import CircleSample
-from .TargetSample import TargetSample
-from .AngleSample import AngleSample
+"""
+Module providing templated samplers to turn dcel components
+into point clouds
+"""
+from .sample_spec import SampleSpec
+from .vector_sample import VectorSample
+from .circle_sample import CircleSample
+from .target_sample import TargetSample
+from .angle_sample import AngleSample
