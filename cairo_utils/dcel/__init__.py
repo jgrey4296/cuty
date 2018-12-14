7af020e8a3e0a9a294edb8e765187c711a997fc9
John Grey <jgrey@ucsc.edu>
2018-12-14 15:50:17 +0000

    fixed imports in dcel init

--- cairo_utils/dcel/__init__.py
+++ cairo_utils/dcel/__init__.py
@@ -1,8 +1,8 @@
 """ General DCEL Module """
 from .dcel import DCEL
-from .Face import Face
-from .HalfEdge import HalfEdge
-from .Vertex import Vertex
+from .face import Face
+from .halfedge import HalfEdge
+from .vertex import Vertex
 from .constants import FaceE, EdgeE, VertE, EditE
 from . import dcel_drawing as drawing
 
