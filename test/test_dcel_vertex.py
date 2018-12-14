0d65a0e7409eaec0756944c0e28ba7f5ca586420
John Grey <jgrey@ucsc.edu>
2018-12-14 15:57:48 +0000

    updated vertex tests

--- test/test_dcel_vertex.py
+++ test/test_dcel_vertex.py
@@ -37,7 +37,7 @@ class DCEL_VERTEX_Tests(unittest.TestCase):
 
     def test_dcel_created_vertex(self):
         """ Create a vertex through the dcel """
-        aDCVert = self.dc.newVertex(np.array([0.2, 0.6]), data={"Test": 5})
+        aDCVert = self.dc.new_vertex(np.array([0.2, 0.6]), data={"Test": 5})
         self.assertIsInstance(aDCVert, dcel.Vertex)
         self.assertEqual(aDCVert.loc[0], 0.2)
         self.assertEqual(aDCVert.loc[1], 0.6)
@@ -46,20 +46,20 @@ class DCEL_VERTEX_Tests(unittest.TestCase):
 
     def test_dcel_created_duplicate_guard(self):
         """ Check duplicate vertices aren't created, and instead vertices are reused """
-        v1 = self.dc.newVertex(np.array([0.2, 0.6]))
-        v2 = self.dc.newVertex(np.array([0.2, 0.6]))
+        v1 = self.dc.new_vertex(np.array([0.2, 0.6]))
+        v2 = self.dc.new_vertex(np.array([0.2, 0.6]))
         self.assertEqual(v1.index, v2.index)
 
     def test_dcel_created_duplicate_integers(self):
         """ Check vertex creation works with integers """
-        v1 = self.dc.newVertex(np.array([3,3]))
-        v2 = self.dc.newVertex(np.array([3,3]))
+        v1 = self.dc.new_vertex(np.array([3,3]))
+        v2 = self.dc.new_vertex(np.array([3,3]))
         self.assertEqual(v1.index, v2.index)
         
     def test_vertex_export_import(self):
         """ Check the exported vertex data has a minimum set of fields """
         exported = self.v._export()
-        self.assertTrue(all([x in exported for x in ["i","x","y","halfEdges","enumData","nonEnumData","active"]]))
+        self.assertTrue(all([x in exported for x in ["i","x","y","half_edges","enum_data","non_enum_data","active"]]))
 
     def test_vertex_activation(self):
         """ Check vertices can be activated and deactivated """
@@ -71,17 +71,17 @@ class DCEL_VERTEX_Tests(unittest.TestCase):
 
     def test_vertex_edge_registration_and_edgeless(self):
         """ Check vertices register halfedges correctly """
-        self.assertTrue(self.v.isEdgeless())
+        self.assertTrue(self.v.is_edgeless())
         he1 = dcel.HalfEdge()
         he2 = dcel.HalfEdge()
-        self.v.registerHalfEdge(he1)
-        self.assertFalse(self.v.isEdgeless())
-        self.v.registerHalfEdge(he2)
-        self.assertFalse(self.v.isEdgeless())
-        self.v.unregisterHalfEdge(he2)
-        self.assertFalse(self.v.isEdgeless())
-        self.v.unregisterHalfEdge(he1)
-        self.assertTrue(self.v.isEdgeless())
+        self.v.register_half_edge(he1)
+        self.assertFalse(self.v.is_edgeless())
+        self.v.register_half_edge(he2)
+        self.assertFalse(self.v.is_edgeless())
+        self.v.unregister_half_edge(he2)
+        self.assertFalse(self.v.is_edgeless())
+        self.v.unregister_half_edge(he1)
+        self.assertTrue(self.v.is_edgeless())
 
     def test_vertex_bboxs(self):
         """ Check vertices can create a bbox around themselves """
@@ -112,18 +112,18 @@ class DCEL_VERTEX_Tests(unittest.TestCase):
     def test_vertex_get_nearby(self):
         """ Check a vertex can call through to its dcel to get nearby vertices """
         #create multiple vertices, get all nearby
-        v1 = self.dc.newVertex(np.array([0.2, 0.6]))
-        self.dc.newVertex(np.array([0.3,0.4]))
-        self.dc.newVertex(np.array([0.1,0.5]))
+        v1 = self.dc.new_vertex(np.array([0.2, 0.6]))
+        self.dc.new_vertex(np.array([0.3,0.4]))
+        self.dc.new_vertex(np.array([0.1,0.5]))
         nearby = v1.get_nearby_vertices(e=1)
         self.assertEqual(len(nearby), 3)
-        self.dc.newVertex(np.array([0.3,0.35]))
+        self.dc.new_vertex(np.array([0.3,0.35]))
         nearby2 = v1.get_nearby_vertices(e=1)
         self.assertEqual(len(nearby2), 4)
 
     def test_vertex_to_array(self):
         """ Get a vertex's coordinates as a np.ndarray """
-        arr = self.v.toArray()
+        arr = self.v.to_array()
         self.assertIsInstance(arr, np.ndarray)
         self.assertEqual(arr.shape, (2,))
         self.assertTrue(all(arr == np.array([0.2, 0.6])))
@@ -131,19 +131,19 @@ class DCEL_VERTEX_Tests(unittest.TestCase):
     def test_vertex_extend_to_line(self):
         """ Check a vertex can be extended to create a line """
         #create a line appropriately
-        v1 = self.dc.newVertex(np.array([0,0]))
+        v1 = self.dc.new_vertex(np.array([0,0]))
         e = v1.extend_line_to(target=np.array([1,0]))
         self.assertIsNotNone(e)
         self.assertIsInstance(e, dcel.HalfEdge)
-        self.assertTrue(all(e.twin.origin.toArray() == np.array([1,0])))
-        self.assertTrue(all(e.origin.toArray() == np.array([0,0])))
-        self.assertFalse(v1.isEdgeless())
+        self.assertTrue(all(e.twin.origin.to_array() == np.array([1,0])))
+        self.assertTrue(all(e.origin.to_array() == np.array([0,0])))
+        self.assertFalse(v1.is_edgeless())
         
     def test_vertex_get_sorted_edges(self):
         """ Check edges from a vertex can be sorted ccw and returned """
         #create multiple lines from,
         #return in sorted order ccw
-        v1 = self.dc.newVertex(np.array([0,0]))
+        v1 = self.dc.new_vertex(np.array([0,0]))
         coords = np.array([[1,0],[0.5, 0.5], [0, 1],
                            [-0.5, 0.5], [-1,0], [-0.5, -0.5],
                            [0.5, -0.5]])
@@ -157,121 +157,121 @@ class DCEL_VERTEX_Tests(unittest.TestCase):
 
     def test_has_constraints_false(self):
         """ A Vertex doesn't have constraints by default """
-        v1 = self.dc.newVertex(np.array([0,0]))
+        v1 = self.dc.new_vertex(np.array([0,0]))
         self.assertFalse(v1.has_constraints())
 
     def test_has_constraints_false_candidates(self):
         """ a vertex doesn't have a constraint with non-related candidates  """
-        v1 = self.dc.newVertex(np.array([0,0]))
+        v1 = self.dc.new_vertex(np.array([0,0]))
         self.assertFalse(v1.has_constraints(set(["a"])))
 
     def test_has_constraints_false(self):
         """ a vertex doesn't have a constraint with a halfedge passed in as a candidate """
-        v1 = self.dc.newVertex(np.array([0,0]))
-        v2 = self.dc.newVertex(np.array([0,0]))
-        e = self.dc.newEdge(v1, v2)
+        v1 = self.dc.new_vertex(np.array([0,0]))
+        v2 = self.dc.new_vertex(np.array([0,0]))
+        e = self.dc.new_edge(v1, v2)
         self.assertFalse(v1.has_constraints(set([e, e.twin])))
 
     def test_has_constraints_true(self):
         """ A vertex does have a constraint if the halfedge is not passed in as a candidate """
-        v1 = self.dc.newVertex(np.array([0,0]))
-        v2 = self.dc.newVertex(np.array([0,0]))
-        e = self.dc.newEdge(v1, v2)
+        v1 = self.dc.new_vertex(np.array([0,0]))
+        v2 = self.dc.new_vertex(np.array([0,0]))
+        e = self.dc.new_edge(v1, v2)
         self.assertTrue(v1.has_constraints())
 
     def test_has_constraints_two_edges_false(self):
         """ a vertex with multiple edges isnt constrained if both are passed in as candidates """  
-        v1 = self.dc.newVertex(np.array([0,0]))
-        v2 = self.dc.newVertex(np.array([0,0]))
-        e = self.dc.newEdge(v1, v2)
-        e2 = self.dc.newEdge(v1,v2)
+        v1 = self.dc.new_vertex(np.array([0,0]))
+        v2 = self.dc.new_vertex(np.array([0,0]))
+        e = self.dc.new_edge(v1, v2)
+        e2 = self.dc.new_edge(v1,v2)
         self.assertFalse(v1.has_constraints(set([e, e2, e.twin, e2.twin])))
 
     def test_has_constraints_two_edges_true(self):
         """ a vertex with multiple edges is constrained if not all its edges as candidates """  
-        v1 = self.dc.newVertex(np.array([0,0]))
-        v2 = self.dc.newVertex(np.array([0,0]))
-        e = self.dc.newEdge(v1, v2)
-        e2 = self.dc.newEdge(v1,v2)
+        v1 = self.dc.new_vertex(np.array([0,0]))
+        v2 = self.dc.new_vertex(np.array([0,0]))
+        e = self.dc.new_edge(v1, v2)
+        e2 = self.dc.new_edge(v1,v2)
         self.assertTrue(v1.has_constraints(set([e, e.twin])))
 
     def test_translate_modified(self):
         """ Check a vertex can be translated without creating a new vertex """
-        v1 = self.dc.newVertex(np.array([5,5]))
+        v1 = self.dc.new_vertex(np.array([5,5]))
         v2, edit_e = v1.translate(np.array([1,1]))
         self.assertEqual(edit_e, EditE.MODIFIED)
         self.assertEqual(v1, v2)
-        self.assertTrue(np.allclose(v1.toArray(), np.array([6,6])))
+        self.assertTrue(np.allclose(v1.to_array(), np.array([6,6])))
 
     def test_translate_force_modified(self):
         """ Check translation can be forced to modify the vertex """
-        e = self.dc.createEdge(np.array([5,5]), np.array([6,6]))
+        e = self.dc.create_edge(np.array([5,5]), np.array([6,6]))
         v1 = e.origin
         v2, edit_e = v1.translate(np.array([1,1]), force=True)
         self.assertEqual(edit_e, EditE.MODIFIED)
         self.assertEqual(v1, v2)
-        self.assertTrue(np.allclose(v1.toArray(), np.array([6,6])))
+        self.assertTrue(np.allclose(v1.to_array(), np.array([6,6])))
 
     def test_translate_new(self):
         """ check translate can create a new vertex """
-        e = self.dc.createEdge(np.array([5,5]), np.array([6,6]))
+        e = self.dc.create_edge(np.array([5,5]), np.array([6,6]))
         v1 = e.origin
         v2, edit_e = v1.translate(np.array([1,1]))
         self.assertEqual(edit_e, EditE.NEW)
         self.assertNotEqual(v1, v2)
-        self.assertTrue(np.allclose(v2.toArray(), np.array([6,6])))
-        self.assertTrue(np.allclose(v1.toArray(), np.array([5,5])))
+        self.assertTrue(np.allclose(v2.to_array(), np.array([6,6])))
+        self.assertTrue(np.allclose(v1.to_array(), np.array([5,5])))
         
     def test_translate_modified_by_candidates(self):
         """ check translate can modify if its constraints match the passed in set """
-        e = self.dc.createEdge(np.array([5,5]), np.array([6,6]))
+        e = self.dc.create_edge(np.array([5,5]), np.array([6,6]))
         v1 = e.origin
         v2, edit_e = v1.translate(np.array([1,1]), candidates=set([e, e.twin]))
         self.assertEqual(edit_e, EditE.MODIFIED)
         self.assertEqual(v1, v2)
-        self.assertTrue(np.allclose(v2.toArray(), np.array([6,6])))
+        self.assertTrue(np.allclose(v2.to_array(), np.array([6,6])))
 
     def test_rotate_modified(self):
         """ Check a vertex can be rotated around another point """
-        v1 = self.dc.newVertex(np.array([5,0]))
+        v1 = self.dc.new_vertex(np.array([5,0]))
         v2, edit_e = v1.rotate(np.array([0,0]), radians(90))
         self.assertEqual(edit_e, EditE.MODIFIED)
         self.assertEqual(v1, v2)
-        self.assertTrue(np.allclose(v2.toArray(), np.array([0,5])))
+        self.assertTrue(np.allclose(v2.to_array(), np.array([0,5])))
         
     def test_rotate_new(self):
         """ Check rotate can create a new vertex when constrained """
-        e = self.dc.createEdge(np.array([5,0]), np.array([6,0]))
+        e = self.dc.create_edge(np.array([5,0]), np.array([6,0]))
         v1 = e.twin.origin
         v2, edit_e = v1.rotate(np.array([0,0]), radians(90))
         self.assertEqual(edit_e, EditE.NEW)
         self.assertNotEqual(v1, v2)
-        self.assertTrue(np.allclose(v2.toArray(), np.array([0,6])))
+        self.assertTrue(np.allclose(v2.to_array(), np.array([0,6])))
 
     def test_rotate_modified_force(self):
         """ Check rotate can be forced to modify an existing vertex """
-        e = self.dc.createEdge(np.array([5,0]), np.array([6,0]))
+        e = self.dc.create_edge(np.array([5,0]), np.array([6,0]))
         v1 = e.twin.origin
         v2, edit_e = v1.rotate(np.array([0,0]), radians(90), force=True)
         self.assertEqual(edit_e, EditE.MODIFIED)
         self.assertEqual(v1, v2)
-        self.assertTrue(np.allclose(v2.toArray(), np.array([0,6])))
+        self.assertTrue(np.allclose(v2.to_array(), np.array([0,6])))
 
     def test_rotate_modified_due_to_candidates(self):
         """ check rotate can modify based on passed in candidate set """
-        e = self.dc.createEdge(np.array([5,0]), np.array([6,0]))
+        e = self.dc.create_edge(np.array([5,0]), np.array([6,0]))
         v1 = e.twin.origin
         v2, edit_e = v1.rotate(np.array([0,0]), radians(90), candidates=set([e, e.twin]))
         self.assertEqual(edit_e, EditE.MODIFIED)
         self.assertEqual(v1, v2)
-        self.assertTrue(np.allclose(v2.toArray(), np.array([0,6])))
+        self.assertTrue(np.allclose(v2.to_array(), np.array([0,6])))
 
     def test_lt(self):
-        v1 = self.dc.newVertex(np.array([0,0]))
-        v2 = self.dc.newVertex(np.array([1,0]))
+        v1 = self.dc.new_vertex(np.array([0,0]))
+        v2 = self.dc.new_vertex(np.array([1,0]))
         self.assertTrue(v1 < v2)
         self.assertFalse(v2 < v1)
-        v3 = self.dc.newVertex(np.array([0,1]))
+        v3 = self.dc.new_vertex(np.array([0,1]))
         self.assertTrue(v3 < v1)
         self.assertFalse(v1 < v3)
 
