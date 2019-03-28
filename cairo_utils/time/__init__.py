b30873b37f1875c50f7740cd757b565e162d9921
John Grey <jgrey@ucsc.edu>
2019-03-28 01:10:03 +0000

    moves the PatternSeq import

--- cairo_utils/time/__init__.py
+++ cairo_utils/time/__init__.py
@@ -1,5 +1,4 @@
-from .pattern import Pattern
-from .pattern_seq import PatternSeq
+from .pattern import Pattern, PatternSeq
 from .parser import parse_string
 from .utils import time_str, print_run_pattern, run_pattern
 from fractions import Fraction as Time
